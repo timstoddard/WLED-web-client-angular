f92db4e43196eded99ba0d4e4489c8940da7f136
Tim Stoddard <tim.stoddard2@gmail.com>
2022-01-06 23:15:55 -0800

    add quick stats page

--- src/app/quick-stats/quick-stats.component.ts
+++ src/app/quick-stats/quick-stats.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+/*
+  server.on("/quickStats", HTTP_GET, [](AsyncWebServerRequest *request){
+    String version = (String)VERSION);
+    String uptime = (String)millis());
+    String freeHeap = (String)ESP.getFreeHeap());
+    // now put these all into a json object and return it
+    // request->send(200, "text/plain", (String)VERSION);
+    // request->send(200, "text/plain", (String)millis());
+    // request->send(200, "text/plain", (String)ESP.getFreeHeap());
+  });
+*/
+
+interface QuickStats {
+  version: string;
+  uptime: string;
+  freeHeap: string;
+}
+
+interface QuickStatsItem {
+  name: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-quick-stats',
   templateUrl: './quick-stats.component.html',
   styleUrls: ['./quick-stats.component.scss']
 })
 export class QuickStatsComponent implements OnInit {
+  stats: QuickStats;
+  items: QuickStatsItem[];
 
-  constructor() { }
+  constructor() {
+    this.stats = {
+      version: '0.13.0-b6',
+      uptime: '7382904321',
+      freeHeap: '42TB',
+    };
+    this.items = this.formatItems(this.stats);
+  }
 
   ngOnInit(): void {
   }
 
+  private formatItems(stats: QuickStats): QuickStatsItem[] {
+    return [
+      {
+        name: 'Version',
+        value: stats.version,
+      },
+      {
+        name: 'Uptime',
+        value: stats.uptime,
+      },
+      {
+        name: 'Free Heap Space',
+        value: stats.freeHeap,
+      },
+    ];
+  }
 }
