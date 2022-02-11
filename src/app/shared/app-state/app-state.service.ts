38bc9625f3433ae343784e60e3136ec784ac81b7
Tim Stoddard <tim.stoddard2@gmail.com>
2022-02-11 00:58:17 -0800

    add getters and settings for app state service

--- src/app/shared/app-state/app-state.service.ts
+++ src/app/shared/app-state/app-state.service.ts
@@ -141,19 +141,7 @@ const appStateStore = new Store({ name: 'WLED App State', state, config });
 
 @Injectable({ providedIn: 'root' })
 export class AppStateService {
-  getAppState() {
-    return appStateStore.pipe(select((n) => n));
-  }
-
-  getOn() {
-    return appStateStore.pipe(select((n) => n.state.on));
-  }
-
-  getBrightness() {
-    return appStateStore.pipe(select((n) => n.state.brightness));
-  }
-
-  updateAll = (response: WledApiResponse) => {
+  setAll = (response: WledApiResponse) => {
     appStateStore.update(() => ({
       state: {
         on: response.state.on,
@@ -220,24 +208,91 @@ export class AppStateService {
     }));
   }
 
-  updateOn = (on: AppStateProps['state']['on']) => {
+  // getters
+  getAppState = () => this.selectFromAppState((n) => n);
+  getOn = () => this.selectFromAppState((n) => n.state.on);
+  getBrightness = () => this.selectFromAppState((n) => n.state.brightness);
+  getTransition = () => this.selectFromAppState((n) => n.state.transition);
+  getCurrentPresetId = () => this.selectFromAppState((n) => n.state.currentPresetId);
+  getCurrentPlaylistId = () => this.selectFromAppState((n) => n.state.currentPlaylistId);
+  getNightLight = () => this.selectFromAppState((n) => n.state.nightLight);
+  getUdp = () => this.selectFromAppState((n) => n.state.udp);
+  getLiveViewOverride = () => this.selectFromAppState((n) => n.state.liveViewOverride);
+  getMainSegmentId = () => this.selectFromAppState((n) => n.state.mainSegmentId);
+  getVersionName = () => this.selectFromAppState((n) => n.info.versionName);
+  getVersionId = () => this.selectFromAppState((n) => n.info.versionId);
+  getLedInfo = () => this.selectFromAppState((n) => n.info.ledInfo);
+  // getStr = () => this.selectFromAppState((n) => n.info.str);
+  getName = () => this.selectFromAppState((n) => n.info.name);
+  getUdpPort = () => this.selectFromAppState((n) => n.info.udpPort);
+  getIsLive = () => this.selectFromAppState((n) => n.info.isLive);
+  // getLm = () => this.selectFromAppState((n) => n.info.lm);
+  getSourceIpAddress = () => this.selectFromAppState((n) => n.info.sourceIpAddress);
+  getWebSocketCount = () => this.selectFromAppState((n) => n.info.webSocketCount);
+  getEffectCount = () => this.selectFromAppState((n) => n.info.effectCount);
+  getPaletteCount = () => this.selectFromAppState((n) => n.info.paletteCount);
+  getWifi = () => this.selectFromAppState((n) => n.info.wifi);
+  getFileSystem = () => this.selectFromAppState((n) => n.info.fileSystem);
+  getWledDevicesOnNetwork = () => this.selectFromAppState((n) => n.info.wledDevicesOnNetwork);
+  getPlatform = () => this.selectFromAppState((n) => n.info.platform);
+  getArduinoVersion = () => this.selectFromAppState((n) => n.info.arduinoVersion);
+  getFreeHeapBytes = () => this.selectFromAppState((n) => n.info.freeHeapBytes);
+  getUptimeSeconds = () => this.selectFromAppState((n) => n.info.uptimeSeconds);
+  // getOpt = () => this.selectFromAppState((n) => n.info.opt);
+  getBrand = () => this.selectFromAppState((n) => n.info.brand);
+  getProductName = () => this.selectFromAppState((n) => n.info.productName);
+  getMacAddress = () => this.selectFromAppState((n) => n.info.macAddress);
+  getIpAddress = () => this.selectFromAppState((n) => n.info.ipAddress);
+
+  // setters
+  setOn = (on: AppStateProps['state']['on']) => this.updateState({ on });
+  setBrightness = (brightness: AppStateProps['state']['brightness']) => this.updateState({ brightness });
+  setTransition = (transition: AppStateProps['state']['transition']) => this.updateState({ transition });
+  setCurrentPresetId = (currentPresetId: AppStateProps['state']['currentPresetId']) => this.updateState({ currentPresetId });
+  setCurrentPlaylistId = (currentPlaylistId: AppStateProps['state']['currentPlaylistId']) => this.updateState({ currentPlaylistId });
+  setNightLight = (nightLight: AppStateProps['state']['nightLight']) => this.updateState({ nightLight });
+  setUdp = (udp: AppStateProps['state']['udp']) => this.updateState({ udp });
+  setLiveViewOverride = (liveViewOverride: AppStateProps['state']['liveViewOverride']) => this.updateState({ liveViewOverride });
+  setMainSegmentId = (mainSegmentId: AppStateProps['state']['mainSegmentId']) => this.updateState({ mainSegmentId });
+  setVersionName = (versionName: AppStateProps['info']['versionName']) => this.updateInfo({ versionName });
+  setVersionId = (versionId: AppStateProps['info']['versionId']) => this.updateInfo({ versionId });
+  setLedInfo = (ledInfo: AppStateProps['info']['ledInfo']) => this.updateInfo({ ledInfo });
+  // setStr = (str: AppStateProps['info']['str']) => this.foo2({ str });
+  setName = (name: AppStateProps['info']['name']) => this.updateInfo({ name });
+  setUdpPort = (udpPort: AppStateProps['info']['udpPort']) => this.updateInfo({ udpPort });
+  setIsLive = (isLive: AppStateProps['info']['isLive']) => this.updateInfo({ isLive });
+  // setLm = (lm: AppStateProps['info']['lm']) => this.foo2({ lm });
+  setSourceIpAddress = (sourceIpAddress: AppStateProps['info']['sourceIpAddress']) => this.updateInfo({ sourceIpAddress });
+  setWebSocketCount = (webSocketCount: AppStateProps['info']['webSocketCount']) => this.updateInfo({ webSocketCount });
+  setEffectCount = (effectCount: AppStateProps['info']['effectCount']) => this.updateInfo({ effectCount });
+  setPaletteCount = (paletteCount: AppStateProps['info']['paletteCount']) => this.updateInfo({ paletteCount });
+  setWifi = (wifi: AppStateProps['info']['wifi']) => this.updateInfo({ wifi });
+  setFileSystem = (fileSystem: AppStateProps['info']['fileSystem']) => this.updateInfo({ fileSystem });
+  setWledDevicesOnNetwork = (wledDevicesOnNetwork: AppStateProps['info']['wledDevicesOnNetwork']) => this.updateInfo({ wledDevicesOnNetwork });
+  setPlatform = (platform: AppStateProps['info']['platform']) => this.updateInfo({ platform });
+  setArduinoVersion = (arduinoVersion: AppStateProps['info']['arduinoVersion']) => this.updateInfo({ arduinoVersion });
+  setFreeHeapBytes = (freeHeapBytes: AppStateProps['info']['freeHeapBytes']) => this.updateInfo({ freeHeapBytes });
+  setUptimeSeconds = (uptimeSeconds: AppStateProps['info']['uptimeSeconds']) => this.updateInfo({ uptimeSeconds });
+  // setOpt = (opt: AppStateProps['info']['opt']) => this.foo2({ opt });
+  setBrand = (brand: AppStateProps['info']['brand']) => this.updateInfo({ brand });
+  setProductName = (productName: AppStateProps['info']['productName']) => this.updateInfo({ productName });
+  setMacAddress = (macAddress: AppStateProps['info']['macAddress']) => this.updateInfo({ macAddress });
+  setIpAddress = (ipAddress: AppStateProps['info']['ipAddress']) => this.updateInfo({ ipAddress });
+
+  private selectFromAppState = (selectFn: (state: AppStateProps) => unknown) =>
+    appStateStore.pipe(select(selectFn));
+
+  private updateState = (newState: Partial<AppStateProps['state']>) => {
     appStateStore.update((n) => ({
-      state: { ...n.state, on },
+      state: { ...n.state, ...newState },
       info: n.info,
     }));
   }
 
-  updateBrightness = (brightness: AppStateProps['state']['brightness']) => {
+  private updateInfo = (newInfo: Partial<AppStateProps['info']>) => {
     appStateStore.update((n) => ({
-      state: { ...n.state, brightness },
-      info: n.info,
-    }));
-  }
-
-  updateTransition = (transition: AppStateProps['state']['transition']) => {
-    appStateStore.update((n) => ({
-      state: { ...n.state, transition },
-      info: n.info,
+      state: n.state,
+      info: { ...n.info, ...newInfo },
     }));
   }
 }
