2a0d0aee8165711e99b50e8a9d62bb722ccad8cc
Tim Stoddard <tim.stoddard2@gmail.com>
2022-10-20 01:44:02 -0700

    feat(controls): :sparkles: add marching ants border around device selector while loading

--- src/app/controls-wrapper/top-menu-bar/device-selector/device-selector.component.ts
+++ src/app/controls-wrapper/top-menu-bar/device-selector/device-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ApiService } from '../../../shared/api.service';
 import { NO_DEVICE_IP_SELECTED } from '../../../shared/app-state/app-state-defaults';
@@ -7,15 +7,18 @@ import { WledIpAddress } from '../../../shared/app-types';
 import { FormService } from '../../../shared/form-service';
 import { UnsubscriberComponent } from '../../../shared/unsubscribing/unsubscriber.component';
 
+type ConnectionStatus = 'connected' | 'disconnected' | 'loading';
+
 @Component({
   selector: 'app-device-selector',
   templateUrl: './device-selector.component.html',
   styleUrls: ['./device-selector.component.scss']
 })
 export class DeviceSelectorComponent extends UnsubscriberComponent implements OnInit {
+  @Input() selectClass: string = '';
   wledIpAddresses!: WledIpAddress[];
   selectedWledIpAddress!: FormControl;
-  connectionStatus!: 'connected' | 'disconnected' | null;
+  connectionStatus!: ConnectionStatus | null;
 
   constructor(
     private appStateService: AppStateService,
@@ -45,20 +48,34 @@ export class DeviceSelectorComponent extends UnsubscriberComponent implements On
       });
   }
 
+  getModifierClass() {
+    switch (this.connectionStatus) {
+      case 'connected':
+        return 'deviceSelector--connected';
+      case 'disconnected':
+        return 'deviceSelector--disconnected';
+      case 'loading':
+        return 'deviceSelector--loading';
+      default:
+        return '';
+    }
+  }
+
   private createControl() {
     return this.formService.createFormControl('');
   }
 
   private setSelectedDevice = (ipAddress: string) => {
     const name = this.wledIpAddresses
-      .find(({ ipv4Address }) => ipAddress === ipv4Address)!
+      .find(({ ipv4Address }) => ipv4Address === ipAddress)!
       .name;
     console.log('wledIpAddress', { name, ipAddress });
-    this.connectionStatus = null;
     if (ipAddress === NO_DEVICE_IP_SELECTED.ipv4Address) {
       // 'None' selected
+      this.connectionStatus = null;
       this.appStateService.setSelectedWledIpAddress(NO_DEVICE_IP_SELECTED);
     } else {
+      this.connectionStatus = 'loading';
       // test ip address as base url before setting
       this.handleUnsubscribe(this.apiService.testIpAddressAsBaseUrl(ipAddress))
         .subscribe(result => {
