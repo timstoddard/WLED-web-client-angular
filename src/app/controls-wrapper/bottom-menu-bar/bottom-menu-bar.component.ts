26a2c6f9040f6a492525c5ea96b69ad59de1b067
Tim Stoddard <tim.stoddard2@gmail.com>
2023-05-06 12:41:07 -0700

    fix router link active issue with bottom menu bar

--- src/app/controls-wrapper/bottom-menu-bar/bottom-menu-bar.component.ts
+++ src/app/controls-wrapper/bottom-menu-bar/bottom-menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UIConfigService } from '../../shared/ui-config.service';
 import { UnsubscriberComponent } from '../../shared/unsubscriber/unsubscriber.component';
 
@@ -8,32 +8,36 @@ import { UnsubscriberComponent } from '../../shared/unsubscriber/unsubscriber.co
   styleUrls: ['./bottom-menu-bar.component.scss']
 })
 export class BottomMenuBarComponent extends UnsubscriberComponent implements OnInit {
-  @Input() pcMode: boolean = false; // TODO is this needed?
   showLabels!: boolean;
   buttons = [
     {
       name: 'Segments',
       routerLink: ['/controls', 'segments'],
+      routerLinkActiveExactMatch: false,
       icon: 'category',
     },
     {
       name: 'Presets',
       routerLink: ['/controls', 'presets'],
+      routerLinkActiveExactMatch: false,
       icon: 'favorite',
     },
     {
       name: 'Controls',
       routerLink: ['/controls'],
+      routerLinkActiveExactMatch: true,
       icon: 'tune',
     },
     {
       name: 'Info',
       routerLink: ['/controls', 'info'],
+      routerLinkActiveExactMatch: false,
       icon: 'info',
     },
     {
       name: 'Settings',
       routerLink: ['/controls', 'settings'],
+      routerLinkActiveExactMatch: false,
       icon: 'settings',
     },
   ];
